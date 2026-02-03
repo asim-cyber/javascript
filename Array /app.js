afac48d1092998c644dc9e59182d0d811470cadd
asim-cyber <mashahwani8@gmail.com>
2026-02-03 13:45:46 +0500

    initial

---
+++ Array /app.js
@@ -0,0 +1,131 @@
+// // // Question no 1
+// let students = ["Ali", "Ahmed", "Sara"];
+// students.push("Asim");
+
+// console.log(students);
+
+
+// // Question no 2
+// // // let fruits = ["Apple", "Banana", "Mango"];
+// // // fruits.pop();
+
+// // // console.log(fruits);
+
+// // Question no 3
+// // // let days = ["Tuesday", "Wednesday", "Thursday"];
+// // // days.unshift("Monday");
+
+// // // console.log(days);
+
+// // Question no 4
+// // // let colors = ["Red", "Blue", "Green"];
+// // // colors.shift();
+
+// // // console.log(colors);
+
+
+// Question no 5
+// let subjects = ["English", "Urdu", "Computer"];
+// subjects.splice(2, 0, "Math", "Science");
+
+// console.log(subjects);
+
+// // Question no 6
+// let cities = ["Karachi", "Lahore", "Quetta", "Peshawar"];
+// cities.splice(1, 2);
+
+// console.log(cities);
+
+
+// // Question no 7
+// // // let countries = ["Pakistan", "India", "China", "Japan", "Turkey"];
+// // // let firstThree = countries.slice(0, 3);
+
+// // // console.log(firstThree);
+
+
+// // Question no 8
+// // // let fruitsString = "apple,banana,mango";
+// // // let fruitsArray = fruitsString.split(",");
+
+// // // console.log(fruitsArray);
+
+// // Question no 9
+// // // let chars = ["J", "S"];
+// // // let joined = chars.join("");
+
+// // // console.log(joined);
+
+// // Question no 10
+// // // let items1 = ["pen", "pencil"];
+// // // let items2 = ["eraser", "sharpener"];
+
+// // // let allItems = items1.concat(items2);
+
+// // // console.log(allItems);
+
+// // Question no 11
+// // // let numbers = [10, 5, 2, 8];
+// // // numbers.sort();
+
+// // // console.log(numbers);
+
+// // Question no 12
+// // // let names = ["Zara", "Ali", "Hamza", "Bilal"];
+// // // names.sort();
+
+// // // console.log(names);
+
+// // Question no 13
+// // // let nums = [1, 2, 3, 4, 5];
+// // // nums.reverse();
+
+// // // console.log(nums);
+
+// // Question no 14
+// // // let moreColors = ["Red", "Green", "Blue", "Yellow"];
+// // // let index = moreColors.indexOf("Blue");
+
+// // // console.log(index);
+
+// // Question no 15
+// // // let cars = [];
+
+// // // cars.push("Honda");
+// // // cars.push("Toyota");
+// // // cars.push("Suzuki");
+
+// // // console.log(cars);
+
+// // Question no 16
+// // // let fiveNums = [10, 20, 30, 40, 50];
+// // // fiveNums.splice(2, 1);
+
+// // // console.log(fiveNums);
+
+// // Question no 17
+// // // let web = "HTML CSS JavaScript";
+// // // let webArray = web.split(" ");
+
+// // // console.log(webArray);
+
+// // Question no 18
+// // // let letters = ["a", "b", "c"];
+// // // let result = letters.reverse().join("-");
+
+// // // console.log(result);
+
+// // Question no 19
+// // // let pakCities = ["Lahore", "Karachi", "Islamabad"];
+// // // let karachiIndex = pakCities.indexOf("Karachi");
+
+// // // console.log(karachiIndex);
+
+// // Question no 20
+// // // let even = [2, 4, 6, 8];
+// // // let odd = [1, 3, 5, 7];
+
+// // // let combined = even.concat(odd);
+// // // combined.reverse();
+
+// // // console.log(combined);
