383d3490e420295059338dc4a7dab5dd07be67ce
asim-cyber <mashahwani8@gmail.com>
2026-02-03 14:13:35 +0500

    initial

---
+++ loops/app.js
@@ -0,0 +1,378 @@
+// // // // Question no 1
+// // let fruits = ["apple", "banana", "cherry"];
+
+// // for (let i = 0; i < fruits.length; i++) {
+// //   console.log(fruits[i]);
+// // }
+
+
+// // // Question no 2
+// // // // let colors = ["red", "green", "blue"];
+// // // // let colorText = "";
+
+// // // // for (let i = 0; i < colors.length; i++) {
+// // // //   colorText += colors[i] + " ";
+// // // // }
+
+// // // // document.querySelector("#output").innerText = colorText;
+
+// // // Question no 3
+// // // // for (let i = 1; i <= 10; i++) {
+// // // //   console.log(i);
+// // // // }
+
+// // // Question no 4
+// // // // let num = 5;
+
+// // // // while (num >= 1) {
+// // // //   console.log(num);
+// // // //   num--;
+// // // // }
+
+// // // Question no 5
+// // // // let names = ["Ali", "Sara", "Ahmed"];
+// // // // document.querySelector("#names").innerText = names.join(", ");
+
+// // // Question no 6
+// // // // document.querySelector("#status").innerText = "Loading...";
+
+// // // Question no 7
+// // // // let numbers = [5, 10, 15];
+// // // // let sum = 0;
+
+// // // // for (let i = 0; i < numbers.length; i++) {
+// // // //   sum += numbers[i];
+// // // // }
+
+// // // // document.querySelector("#sum").innerText = sum;
+
+// // // Question no 8
+// // // // let cities = ["Karachi", "Lahore", "Quetta"];
+// // // // document.querySelector("#cities").innerText = cities.join(", ");
+
+// // // Question no 9
+// // // // for (let i = 1; i <= 20; i++) {
+// // // //   if (i % 2 === 0) {
+// // // //     console.log(i);
+// // // //   }
+// // // // }
+
+// // // Question no 10
+// let nums = [45, 22, 89, 100];
+// let largest = nums[0];
+
+// for (let i = 1; i < nums.length; i++) {
+//   if (nums[i] > largest) {
+//     largest = nums[i];
+//   }
+// }
+
+// document.querySelector("#max").innerText = largest;
+
+// // // Question no 11
+// // // // document.querySelector("#result").innerText = "Task Complete";
+
+
+// // // Question no 12
+// // // // let temps = [30, 35, 32, 40];
+// // // // let totalTemp = 0;
+
+// // // // for (let i = 0; i < temps.length; i++) {
+// // // //   totalTemp += temps[i];
+// // // // }
+
+// // // // let avgTemp = totalTemp / temps.length;
+// // // // document.querySelector("#averageTemp").innerText = avgTemp;
+
+// // // Question no 13
+// // // // let total = 0;
+
+// // // // for (let i = 1; i <= 100; i++) {
+// // // //   total += i;
+// // // // }
+
+// // // // document.querySelector("#totalSum").innerText = total;
+
+// // // Question no 14
+// // // // let animals = ["dog", "cat", "bird"];
+// // // // document.querySelector("#animals").innerText = animals.join(", ");
+
+// // // Question no 15
+// // // // document.querySelector("#title").innerText = "Welcome to JavaScript!";
+
+// // // Question no 16
+// // // // for (let i = 1; i <= 50; i++) {
+// // // //   if (i % 5 === 0) {
+// // // //     console.log(i);
+// // // //   }
+// // // // }
+
+// // // Question no 17
+// // // // let scores = [80, 60, 90, 70];
+// // // // let count = 0;
+
+// // // // for (let i = 0; i < scores.length; i++) {
+// // // //   if (scores[i] > 75) {
+// // // //     count++;
+// // // //   }
+// // // // }
+
+// // // // document.querySelector("#highScores").innerText = count;
+
+// // // Question no 18
+// // // // document.querySelector("#message").innerText = "Updated successfully";
+
+
+// // // Question no 19
+// // // // let subjects = ["Math", "Science", "English"];
+// // // // document.querySelector("#subjectsList").innerText = subjects.join(", ");
+
+
+// // // Question no 20 + 21
+// // // // let arr = [5, 10, 15, 20];
+// // // // let arrSum = 0;
+// // // // let maxNum = arr[0];
+
+// // // // for (let i = 0; i < arr.length; i++) {
+// // // //   arrSum += arr[i];
+
+// // // //   if (arr[i] > maxNum) {
+// // // //     maxNum = arr[i];
+// // // //   }
+// // // // }
+
+// // // // console.log("Sum:", arrSum);
+// // // // console.log("Largest:", maxNum);
+
+
+// // // Question no 22
+// // // // let evenArr = [3, 8, 6, 1, 9, 2];
+// // // // let evenCount = 0;
+
+// // // // for (let i = 0; i < evenArr.length; i++) {
+// // // //   if (evenArr[i] % 2 === 0) {
+// // // //     evenCount++;
+// // // //   }
+// // // // }
+
+// // // // console.log("Even numbers:", evenCount);
+
+
+// // // Question no 23
+// // // // let avgArr = [20, 30, 40, 50];
+// // // // let avgSum = 0;
+
+// // // // for (let i = 0; i < avgArr.length; i++) {
+// // // //   avgSum += avgArr[i];
+// // // // }
+
+// // // // console.log("Average:", avgSum / avgArr.length);
+
+// // // Question no 24
+// // // // let multi = [2, 4, 6, 8, 10];
+// // // // let newArr = [];
+
+// // // // for (let i = 0; i < multi.length; i++) {
+// // // //   newArr.push(multi[i] * 3);
+// // // // }
+
+// // // // console.log(newArr);
+
+// // // Question no 25
+// // // // let greater = [1, 3, 5, 7, 9];
+// // // // let greaterCount = 0;
+
+// // // // for (let i = 0; i < greater.length; i++) {
+// // // //   if (greater[i] > 4) {
+// // // //     greaterCount++;
+// // // //   }
+// // // // }
+
+// // // // console.log(greaterCount);
+
+// // // Question no 26
+// // // // let divFive = [10, 15, 20, 25, 30];
+// // // // let divSum = 0;
+
+// // // // for (let i = 0; i < divFive.length; i++) {
+// // // //   if (divFive[i] % 5 === 0) {
+// // // //     divSum += divFive[i];
+// // // //   }
+// // // // }
+
+// // // // console.log(divSum);
+
+// // // Question no 27
+// // // // let checkNum = [10, 20, 30, 40, 25];
+// // // // let exists = false;
+
+// // // // for (let i = 0; i < checkNum.length; i++) {
+// // // //   if (checkNum[i] === 25) {
+// // // //     exists = true;
+// // // //   }
+// // // // }
+
+// // // // console.log(exists);
+
+// // // Question no 28
+// // // // let a1 = [1, 2, 3];
+// // // // let a2 = [4, 5, 6];
+// // // // let a3 = [];
+
+// // // // for (let i = 0; i < a1.length; i++) {
+// // // //   a3.push(a1[i] + a2[i]);
+// // // // }
+
+// // // // console.log(a3);
+
+// // // Question no 29
+// // // // let secArr = [10, 99, 23, 87, 56];
+// // // // let first = 0;
+// // // // let second = 0;
+
+// // // // for (let i = 0; i < secArr.length; i++) {
+// // // //   if (secArr[i] > first) {
+// // // //     second = first;
+// // // //     first = secArr[i];
+// // // //   } else if (secArr[i] > second) {
+// // // //     second = secArr[i];
+// // // //   }
+// // // // }
+
+// // // // console.log("Second Largest:", second);
+
+// // // Question no 30
+// // // // let rev = [1, 2, 3, 4, 5];
+// // // // let reversed = [];
+
+// // // // for (let i = rev.length - 1; i >= 0; i--) {
+// // // //   reversed.push(rev[i]);
+// // // // }
+
+// // // // console.log(reversed);
+
+// // // Question no 31
+// // // // for (let i = 2; i <= 50; i++) {
+
+// // // //   let prime = true;
+
+// // // //   for (let j = 2; j < i; j++) {
+// // // //     if (i % j === 0) {
+// // // //       prime = false;
+// // // //       break;
+// // // //     }
+// // // //   }
+
+// // // //   if (prime) {
+// // // //     console.log(i);
+// // // //   }
+// // // // }
+
+// // // Question no 32
+// // // // let diffArr = [11, 4, 99, 2, 47];
+// // // // let max = diffArr[0];
+// // // // let min = diffArr[0];
+
+// // // // for (let i = 0; i < diffArr.length; i++) {
+
+// // // //   if (diffArr[i] > max) {
+// // // //     max = diffArr[i];
+// // // //   }
+
+// // // //   if (diffArr[i] < min) {
+// // // //     min = diffArr[i];
+// // // //   }
+// // // // }
+
+// // // // console.log("Difference:", max - min);
+
+// // // Question no 33
+// // // // let negArr = [0, -1, -5, 6, 8, -3];
+// // // // let negCount = 0;
+
+// // // // for (let i = 0; i < negArr.length; i++) {
+// // // //   if (negArr[i] < 0) {
+// // // //     negCount++;
+// // // //   }
+// // // // }
+
+// // // // console.log(negCount);
+
+// // // Question no 34
+// // // // let mix = [1,2,3,4,5,6,7];
+// // // // let odd = [];
+
+// // // // for (let i = 0; i < mix.length; i++) {
+// // // //   if (mix[i] % 2 !== 0) {
+// // // //     odd.push(mix[i]);
+// // // //   }
+// // // // }
+
+// // // // console.log(odd);
+
+// // // Question no 35
+// // // // let zeroArr = [0, 1, 0, 3, 0, 5];
+// // // // let noZero = [];
+
+// // // // for (let i = 0; i < zeroArr.length; i++) {
+// // // //   if (zeroArr[i] !== 0) {
+// // // //     noZero.push(zeroArr[i]);
+// // // //   }
+// // // // }
+
+// // // // console.log(noZero);
+
+
+// // // Question no 36
+// // // // let swap = [1,2,3,4,5,6];
+
+// // // // let temp = swap[0];
+// // // // swap[0] = swap[swap.length - 1];
+// // // // swap[swap.length - 1] = temp;
+
+// // // // console.log(swap);
+
+// // // Question no 37
+// // // // let allEven = [2,4,6,8];
+// // // // let isAllEven = true;
+
+// // // // for (let i = 0; i < allEven.length; i++) {
+// // // //   if (allEven[i] % 2 !== 0) {
+// // // //     isAllEven = false;
+// // // //   }
+// // // // }
+
+// // // // console.log(isAllEven);
+
+
+// // // Question no 38
+// // // // let avgCheck = [5,10,15,20,25];
+// // // // let sumCheck = 0;
+
+// // // // for (let i = 0; i < avgCheck.length; i++) {
+// // // //   sumCheck += avgCheck[i];
+// // // // }
+
+// // // // let avgValue = sumCheck / avgCheck.length;
+// // // // let countAvg = 0;
+
+// // // // for (let i = 0; i < avgCheck.length; i++) {
+// // // //   if (avgCheck[i] >= avgValue) {
+// // // //     countAvg++;
+// // // //   }
+// // // // }
+
+// // // // console.log(countAvg);
+
+// // // Question no 39
+// // // // let equalArr = [2,2,2,2,2];
+// // // // let allEqual = true;
+
+// // // // for (let i = 1; i < equalArr.length; i++) {
+// // // //   if (equalArr[i] !== equalArr[0]) {
+// // // //     allEqual = false;
+// // // //   }
+// // // // }
+
+// // // // console.log(allEqual);
+
